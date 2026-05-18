2a44d7d52afa08d27e7572c626a09e2ecb7db24f
Pierre-Yves <pierre-yves.monnet@laposte.net>
2026-05-18 16:47:35 -0700

    Basic class to run the connector

---
+++ src/test/java/io/camunda/jasper/ElementTemplateGenerator.java
@@ -0,0 +1,24 @@
+package io.camunda.jasper;
+
+import io.camunda.cherry.definition.RunnerDecorationTemplate;
+import io.camunda.connector.jasper.JasperFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ElementTemplateGenerator {
+    private static final Logger logger = LoggerFactory.getLogger(ElementTemplateGenerator.class.getName());
+
+    public static void generate() {
+        // Call the Cherry runtime
+        try {
+            RunnerDecorationTemplate runnerDecorationTemplate = new RunnerDecorationTemplate(new JasperFunction());
+            runnerDecorationTemplate.generateElementTemplate("./element-templates/", "jasper-function.json");
+        } catch (Exception e) {
+            logger.error("Error during generation", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        generate();
+    }
+}
